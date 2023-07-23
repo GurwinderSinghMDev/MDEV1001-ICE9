28bc1f223d092c1ff527d05ee572d85b3321689b
GurwinderSinghMDev <133176884+GurwinderSinghMDev@users.noreply.github.com>
2023-07-23 12:43:20 -0400

    Files for ICE9 IOS part

---
+++ MDEV1001-ICE9/MDEV1001-ICE9/Movie.swift
@@ -0,0 +1,22 @@
+//
+//  Movie.swift
+//  MDEV1001-ICE7
+//
+//  Created by Gurwinder Singh on 2023-07-06.
+//
+struct Movie: Codable
+{
+    let _id: String
+    let movieID: String
+    let title: String
+    let studio: String
+    let genres: [String]
+    let directors: [String]
+    let writers: [String]
+    let actors: [String]
+    let year: Int
+    let length: Int
+    let shortDescription: String
+    let mpaRating: String
+    let criticsRating: Double
+}
---
+++ MDEV1001-ICE9/MDEV1001-ICE9/MovieTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTableViewCell.swift
+//  MDEV1001-ICE9
+//
+//  Created by Gurwinder Singh on 2023-07-06.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UITextField!
+    
+    @IBOutlet weak var studioLabel: UITextField!
+    
+    @IBOutlet weak var ratingLabel: UITextField!
+}
